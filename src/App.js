77a99ebd08daff92ec1223d38c246724d1b945e1
GraceYu <librayuyueran@gmail.com>
2021-11-03 23:48:46 -0500

    build dynamic routing withing being aware of the entire url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import './App.css';
-import {HomePage} from "./pages/homepages/homepage.component";
+import {Route, Link} from 'react-router-dom';
+
+const HomePage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to='/topics'>Topics</Link>
+      <button onClick={() => props.history.push('/topics')}>Topics2</button>
+    </div>
+  )
+}
+
+const TopicList = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Topic List Page</h1>
+      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link><br/>
+      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link><br/>
+      <Link to={`${props.match.url}/22`}>TO TOPIC 22</Link>
+
+    </div>
+  )
+}
+
+const TopicDetail = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Topic Details PAGE: {props.match.params.topicId}</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/blog/wgwrhtjf/topics' component={TopicList}/>
+      <Route exact path='/blog/wgwrhtjf/topics/:topicId' component={TopicDetail}/>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {DirectoryWrapper} from "./directory.style";
-import MenuItem from "../../pages/menu-item/menu-item.component";
+import MenuItem from "../menu-item/menu-item.component";
 
 class Directory extends React.Component {
   constructor(props) {
