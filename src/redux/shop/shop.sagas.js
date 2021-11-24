d581e1e203e9fcf2befcc4e7c7289afbe441b691
GraceYu <librayuyueran@gmail.com>
2021-11-23 19:04:34 -0600

    shop saga, implement takeEvery, call, put

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,29 @@
+import {takeEvery, call, put} from 'redux-saga/effects';
+import ShopActionTypes from "./shop.types";
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
+import {fetchCollectionsFailure, fetchCollectionsSuccess} from "./shop.actions";
+
+export function* fetchCollectionsAsync() {
+  yield console.log('here is the async fetching')
+  try {
+    const collectionRef = firestore.collection('collections')
+    const snapshot = yield collectionRef.get()
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot)
+    // put out = dispatch out in object
+    yield put(fetchCollectionsSuccess(collectionsMap))
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message))
+  }
+
+  // collectionRef.get().then(snapshot => {
+  //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+  //   dispatch(fetchCollectionsSuccess(collectionsMap))
+  // }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  )
+}
