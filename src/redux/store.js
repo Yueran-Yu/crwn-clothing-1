5067f1a8b8d3b4f99717697b2dfe526ff8ead285
GraceYu <librayuyueran@gmail.com>
2021-11-24 13:02:35 -0600

    add root saga using all(call([singleSagafunction]))

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import {createStore, applyMiddleware} from "redux";
 import {logger} from 'redux-logger';
-import rootReducer from './root-reducer';
 import {persistStore} from "redux-persist";
 import createSagaMiddleware from 'redux-saga';
-import {fetchCollectionsAsync} from "./shop/shop.sagas";
+import rootReducer from './root-reducer';
+import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -11,9 +11,9 @@ const middlewares = [sagaMiddleware]
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
 }
-const store = createStore(rootReducer,  applyMiddleware(...middlewares))
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-sagaMiddleware.run(fetchCollectionsAsync)
+sagaMiddleware.run(rootSaga)
 
 const persistor = persistStore(store)
 
